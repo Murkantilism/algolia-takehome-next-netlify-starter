3c975c15406e69ed5a03fbd1d8fd49b4dbee14de
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2022-04-24 19:03:57 -0400

    Corrected log

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ const ALGOLIA_APP_ID = "QP39CFZYBC";
 const ALGOLIA_PUBLIC_KEY = "38ca11446f95b3a3ce5d4c19ad57e52a";
 const ALGOLIA_INDEX = "netlify_085ff13f-3a74-438b-bd6f-c9295ec4a37d_main_all";
 const ALGOLIA_ADMIN_API_KEY = process.env.ALGOLIA_ADMIN_API_KEY;
-console.log("ALGOLIA_ADMIN_API_KEY: ", ALGOLIA_ADMIN_API_KEY);
+console.log(
+  "ALGOLIA_ADMIN_API_KEY found: ",
+  ALGOLIA_ADMIN_API_KEY ? "Yes" : "No"
+);
 const searchClient = algoliasearch(
   ALGOLIA_APP_ID,
   ALGOLIA_ADMIN_API_KEY ? ALGOLIA_ADMIN_API_KEY : ALGOLIA_PUBLIC_KEY
